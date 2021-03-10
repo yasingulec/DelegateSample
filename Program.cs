c21b196f063b47c4570de565caa8165327a00d28
Yasin <yasin.gulec94@outlook.com>
2021-03-10 10:21:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DelegateSample
+{
+    class Program
+    {
+        public delegate void DrinkingAction();
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = (int a, int b) => a + b;
+            Console.WriteLine(add(10,20));
+            Console.ForegroundColor = ConsoleColor.Green;
+            static void DrinkWater()
+            {
+                Console.WriteLine("Drinking water");
+            }
+            static void Print()
+            {
+                Console.WriteLine("falan filan");
+            }
+
+            Action PrintAction = () =>Console.WriteLine("bir şeyler");
+
+
+            static void DrinkBeer( )
+            {
+                Console.WriteLine("Drinking beer");
+            }
+
+            static void DrinkLemonade()
+            {
+                Console.WriteLine("Drinking lemonade");
+            }
+           static void RelaxingOnBeach(Action PrintAction)
+            {
+                Console.WriteLine($"Relaxing on the beach while drinking");
+                PrintAction();            
+            }
+            RelaxingOnBeach(PrintAction);
+            RelaxingOnBeach(DrinkBeer);
+            RelaxingOnBeach(DrinkLemonade);
+
+        }
+       
+    }
+}
