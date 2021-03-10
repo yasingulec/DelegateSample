f59fcceb59a01a8fa813ac226b0946b42adaa70e
Yasin <yasin.gulec94@outlook.com>
2021-03-10 10:27:03 +0300

    -

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace DelegateSample
 {
     class Program
     {
+        //delegateler fonksiyonlar için bir pointerdir, lamda expression ile anonymous functionları contain edebilirler
         public delegate void DrinkingAction();
         static void Main(string[] args)
         {
+            //func geriye değer döndürür
             Func<int, int, int> add = (int a, int b) => a + b;
             Console.WriteLine(add(10,20));
             Console.ForegroundColor = ConsoleColor.Green;
@@ -19,6 +21,7 @@ namespace DelegateSample
                 Console.WriteLine("falan filan");
             }
 
+            //action geriye değer döndürmeyen delegate
             Action PrintAction = () =>Console.WriteLine("bir şeyler");
 
 
